48c862fd4e4305c6e680560fcca16943023807ae
Jayden <jayden.liwanag@edu.sait.ca>
2024-03-15 23:34:54 -0600

    Title

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Hiragana & Katakana Study",
+  title: "SakuraBeats",
   description: "Developer: Jayden Liwanag",
 };
 
